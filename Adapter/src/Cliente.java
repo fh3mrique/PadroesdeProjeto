ee8b312f30f035ddf22c60da05460960d306df1a
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-11-21 08:12:44 -0300

    fix

--- Adapter/src/Cliente.java
+++ Adapter/src/Cliente.java
@@ -5,13 +5,13 @@ import interfacemoderna.LeitorCartaoModerno;
 
 public class Cliente {
     public static void main(String[] args) throws Exception {
-
+        
         // Usando um leitor de cartão antigo com o adapter
         LeitorCartaoAntigo leitorAntigo = new LeitorCartaoAntigoImpl();
         LeitorCartaoModerno adaptador = new AdaptadorLeitorCartao(leitorAntigo);
 
-        // Agora podemos usar o leitor antigo como se fosse um leitor moderno
         adaptador.lerCartaoModerno();
+
 
     }
 }
